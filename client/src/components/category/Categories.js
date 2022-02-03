23b1e9d1fc668023ba8f1ef4e76fdd79cb61505e
Daniel Williams <dwill8944@gmail.com>
2022-02-02 18:57:00 -0700

    lgtm

--- client/src/components/category/Categories.js
+++ client/src/components/category/Categories.js
@@ -8,51 +8,53 @@ import { Header67 } from '../../styles/CategoryStyles';
 
 
 const Categories = ({ gears, getAllGears}) => { 
-	const [filter, setFilter] = useState('All')
-	
-	const [filterKits, setFilterKits ] = useState([])
+  const [filter, setFilter] = useState('All')
+  
+  const [filterKits, setFilterKits ] = useState([])
 
-	useEffect( () => {
-			getAllGears()
-			visibleItems()
-	}, [])
+  useEffect( () => {
+      getAllGears()
+      visibleItems()
+  }, [])
 
    
-const	visibleItems = () => {
+const visibleItems = () => {
 
     
     switch(filter) {
-			case 'Camera':
-				setFilterKits( gears.filter( g => g.category == 'Camera' ))
-			case 'Audio':
-				setFilterKits( gears.filter( g => g.category == 'Audio' ))
-			case 'Lens':
-				setFilterKits( gears.filter( g => g.category == 'Lens' ))
-			case 'Light':
-				setFilterKits( gears.filter( g => g.category == 'Light' ))
-			default:
-				setFilterKits( gears)
+      case 'Camera':
+        setFilterKits( gears.filter( g => g.category == 'Camera' ))
+      case 'Audio':
+        setFilterKits( gears.filter( g => g.category == 'Audio' ))
+      case 'Lens':
+        setFilterKits( gears.filter( g => g.category == 'Lens' ))
+      case 'Light':
+        setFilterKits( gears.filter( g => g.category == 'Light' ))
+      default:
+        setFilterKits( gears)
     }
-	}
+  }
     
-	return (
-		<>
-			
-			<Header67>
-						
-          		Categories
-						
-						</Header67>
-			<CategoryShow filter={filter} setFilter={setFilter} />
-		</>
-	)
+  return (
+    <>
+      
+      <Header67>
+            
+              Categories
+            
+            </Header67>
+      <CategoryShow filter={filter} setFilter={setFilter} />
+    </>
+  )
 }
 
 
 const ConnectedCategory = (props) => (
-	<GearConsumer>
-		{ value => <Categories {...props} {...value} />}
-	</GearConsumer>
+  <GearConsumer>
+    { value => <Categories {...props} {...value} />}
+  </GearConsumer>
 )
 
 export default ConnectedCategory;
+
+
